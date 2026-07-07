53fb017d3b2a84d51030a9332f41f4e3ba72798e
Joran Vinicíus Silveira Lage <joranvinicius@gmail.com>
2026-07-07 11:28:23 -0300

    4 Novas unidades de teste para UsuarioRepositoryTest
    Test1: Cadastrar usuário para checar se ele esta sendo realmente cadastrado
    Test2: Busca um usuário cadastrado usando o método buscarLogin()
    Test3: Busca um usuario que não existe esperando Null
    Test4: Deleta um usuário cadastrado

--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,54 @@
 package br.com.gerenciamento.repository;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.gerenciamento.model.Usuario;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Test 
+    public void cadastrarUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setUser("usuario1");
+        usuario.setEmail("123@gmail.com");
+        usuario.setSenha("123");
+        usuarioRepository.save(usuario);
+        Assert.assertNotNull(usuario.getId());
+    }
+    
+    @Test
+    public void buscarUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setUser("usuario1");
+        usuario.setEmail("123@gmail.com");
+        usuario.setSenha("123");
+        usuarioRepository.save(usuario);
+        Assert.assertNotNull(usuarioRepository.buscarLogin("usuario1", "123"));
+    }
+
+    @Test
+    public void buscarUsuarioInexistente() {
+        Assert.assertNull(usuarioRepository.buscarLogin("usuarioInexistente", "123"));
+    }
+
+    @Test
+    public void deletarUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setUser("usuario1");
+        usuario.setEmail("123@gmail.com");
+        usuario.setSenha("123");
+        usuarioRepository.save(usuario);
+        usuarioRepository.deleteById(usuario.getId());
+        Assert.assertNull(usuarioRepository.buscarLogin("usuario1", "123"));
+    }
 }
